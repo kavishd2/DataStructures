2351bb1f2b68b6c2c5e00d060c588066800f8887
kavishd2 <109032878+kavishd2@users.noreply.github.com>
2025-06-21 14:54:59 -0500

    Update ExpressionTree.py

--- ExpressionTree.py
+++ ExpressionTree.py
@@ -1,24 +1,12 @@
-#  File: ExpressionTree.py
-#  Student Name: Kavish Dewani
-#  Student UT EID: kd28796
-
 import sys
 
-# list of valid operators
 operators = ['+', '-', '*', '/', '//', '%', '**']
 
-
-# Input: Elements of a simple expression
-#        operator (String) and two operands (numbers)
-# Output: result of evaluation of the expression
+# Returns evaluation given operator and operands
 def operation(operator, n, m):
     expression = str(n) + operator + str(m)
     return eval(expression)
 
-
-# Stack Class - DO NOT CHANGE
-# Traditional Stack implementation containing list of data items
-# Used to keep track of items in nested expressions.
 class Stack (object):
     def __init__(self):
         self.stack = []
@@ -29,11 +17,12 @@ class Stack (object):
     def pop(self):
         if (not self.is_empty()):
             return self.stack.pop()
-        else:
-            return None
+        return None
 
     def peek(self):
-        return self.stack[-1]
+        if (not self.is_empty()):
+            return self.stack[-1]
+        return None
 
     def size(self):
         return len(self.stack)
@@ -41,30 +30,19 @@ class Stack (object):
     def is_empty(self):
         return self.size() == 0
 
-
-# Node Class
-# Purpose: Used by the Tree Class to represent one operand or operators
-#          in a binary expression. It includes data (a character) and
-#          two pointers, to the left and right child nodes.
-# You do not need to make changes to this class.
 class Node(object):
     def __init__(self, data=None, lChild=None, rChild=None):
         self.data = data
         self.lChild = lChild
         self.rChild = rChild
 
-
-# Tree Class
-# Purpose: To represent the string representation of operators and operands
-#          of a binary expression so it can be evaluated.
-# You need to make a lot f changes to this class!
+# Represents expression so that it can be evaluated
 class Tree (object):
     def __init__(self):
         self.root = Node(None)
 
-    # Input: a string expression
-    # Output: an expression tree
-    def create_tree(self, expr): # Keeps adding to the stack till finding a right parentheses to make the parent node
+    # Keeps adding to the stack till finding a right parentheses to make the parent node
+    def create_tree(self, expr):
         stack = Stack()
         tokens = expr.split()
         for token in tokens:
@@ -81,62 +59,43 @@ class Tree (object):
                 stack.push(Node(token))
         self.root = stack.pop()
 
-    # Input: A node in an expression tree
-    # Output: The result of evaluating the expression
-    #         with this node as the root
+    # Result of evaluating the expression with this node as the root
     def evaluate(self, current): # Recursively calls method depending on whether current is an operator or not
         if current.data not in operators:
             return current.data
         return float(operation(current.data, self.evaluate(current.left), self.evaluate(current.right)))
 
-    # Starter Method for pre_order
-    # Input: a node in an expression tree
-    # Output: (string) the preorder notation of the expression
-    #                  with this node a the root
+    # The preorder notation of the expression with this node as the root
     def pre_order(self, current): # Adds operator before operands
         if current.data not in operators:
             return current.data
         return current.data + " " + self.pre_order(current.left) + " " + self.pre_order(current.right)
 
-    # Starter Method for post_order
-    # Input: a node in an expression tree
-    # Output: (string) the post order notation of the expression
-    #                  with this node a the root
+    # The post order notation of the expression with this node as the root
     def post_order(self, current): # Adds operator after operands
         if current.data not in operators:
             return current.data
         return self.post_order(current.left) + " " + self.post_order(current.right) + " " + current.data
 
+# Execution
+debug = False
+if debug:
+    in_data = open('expression.in')
+else:
+    in_data = sys.stdin
 
-''' ##### DRIVER CODE #####
-    ##### Do not change, except for the debug flag '''
+# read infix expression
+line = in_data.readline()
+expr = line.strip()
 
+tree = Tree()
+tree.create_tree(expr)
 
-def main():
+# evaluate the expression and print the result
+print(expr, "=", str(tree.evaluate(tree.root)))
 
-    # Debug flag - set to False before submitting
-    debug = False
-    if debug:
-        in_data = open('expression.in')
-    else:
-        in_data = sys.stdin
+# get the prefix version of the expression and print
+print("Prefix Expression:", tree.pre_order(tree.root).strip())
 
-    # read infix expression
-    line = in_data.readline()
-    expr = line.strip()
-
-    tree = Tree()
-    tree.create_tree(expr)
-
-    # evaluate the expression and print the result
-    print(expr, "=", str(tree.evaluate(tree.root)))
-
-    # get the prefix version of the expression and print
-    print("Prefix Expression:", tree.pre_order(tree.root).strip())
-
-    # get the postfix version of the expression and print
-    print("Postfix Expression:", tree.post_order(tree.root).strip())
-
-
-if __name__ == "__main__":
-    main()
+# get the postfix version of the expression and print
+print("Postfix Expression:", tree.post_order(tree.root).strip())
